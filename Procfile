e36d8c55b0fa4ca33f92f50a21b458fa168b55ad
Angelica Pereira Rocha <angelicarocha@MacBook-Air-de-Angelica.local>
2026-04-21 00:15:44 -0300

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python init_db.py && gunicorn app:app --bind 0.0.0.0:$PORT --workers 2 --timeout 120
+web: gunicorn app:app --bind 0.0.0.0:$PORT --workers 2
--- app.py
+++ app.py
@@ -32,4 +32,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=False, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
